402fe8c88541266053b986e39ddaca9e18866f61
kevinocotan <kaocotan@gmail.com>
2024-11-13 22:17:05 -0600

    retomando login viejko

--- app/views/login.php
+++ app/views/login.php
@@ -1,84 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html class=''>
 
-        <!-- ===== CSS ===== -->
-        <link rel="stylesheet" href="css/login-styles.css">
-    
-        <!-- ===== BOX ICONS ===== -->
-        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URL; ?>public_html/css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <title>MyControl School</title>
+</head>
 
-        <title>Responsive Login Form Sign In Sign Up</title>
-    </head>
-    <body>
-        <div class="login">
-            <div class="login__content">
-                <div class="login__img">
-                    <img src="assets/img/img-login.svg" alt="">
+<body>
+    <div class="wrapper1">
+        <div class="container1">
+
+            <h1>MyControl School</h1>
+
+            <form action="login.php" id="formlogin">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Usuario" name="usuario">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Contraseña" name="password">
+                <div class="alert alert-danger mt-5 d-none" role="alert" id="mensaje">
+                    Bienvenido
                 </div>
+                <button class="btn btn-success" type="submit">Iniciar Sesión</button>
+            </form>
+            <p class="text-muted text-center mt-5">
+                Copyright &copy; 2022
+            </p>
 
-                <div class="login__forms">
-                    <form action="" class="login__registre" id="login-in">
-                        <h1 class="login__title">Sign In</h1>
-    
-                        <div class="login__box">
-                            <i class='bx bx-user login__icon'></i>
-                            <input type="text" placeholder="Username" class="login__input">
-                        </div>
-    
-                        <div class="login__box">
-                            <i class='bx bx-lock-alt login__icon'></i>
-                            <input type="password" placeholder="Password" class="login__input">
-                        </div>
 
-                        <a href="#" class="login__forgot">Forgot password?</a>
-
-                        <a href="#" class="login__button">Sign In</a>
-
-                        <div>
-                            <span class="login__account">Don't have an Account ?</span>
-                            <span class="login__signin" id="sign-up">Sign Up</span>
-                        </div>
-                    </form>
-
-                    <form action="" class="login__create none" id="login-up">
-                        <h1 class="login__title">Create Account</h1>
-    
-                        <div class="login__box">
-                            <i class='bx bx-user login__icon'></i>
-                            <input type="text" placeholder="Username" class="login__input">
-                        </div>
-    
-                        <div class="login__box">
-                            <i class='bx bx-at login__icon'></i>
-                            <input type="text" placeholder="Email" class="login__input">
-                        </div>
-
-                        <div class="login__box">
-                            <i class='bx bx-lock-alt login__icon'></i>
-                            <input type="password" placeholder="Password" class="login__input">
-                        </div>
-
-                        <a href="#" class="login__button">Sign Up</a>
-
-                        <div>
-                            <span class="login__account">Already have an Account ?</span>
-                            <span class="login__signup" id="sign-in">Sign In</span>
-                        </div>
-
-                        <div class="login__social">
-                            <a href="#" class="login__social-icon"><i class='bx bxl-facebook' ></i></a>
-                            <a href="#" class="login__social-icon"><i class='bx bxl-twitter' ></i></a>
-                            <a href="#" class="login__social-icon"><i class='bx bxl-google' ></i></a>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
+        <ul class="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
 
-        <!--===== MAIN JS =====-->
-        <script src="js/main-login.js"></script>
-    </body>
+
+    <script src="<?php echo URL; ?>public_html/customjs/api.js"></script>
+    <script src="<?php echo URL; ?>public_html/customjs/login.js"></script>
+
+</body>
+
 </html>
